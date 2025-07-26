a983867bb8dc32f8cb41094f58e8acb3f8c02677
Frank Elsinga <frank@elsinga.de>
2025-07-26 19:12:13 +0200

    implemented a basic draft of the style-spec

---
+++ martin/src/styles/style_spec/mod.rs
@@ -0,0 +1,15 @@
+mod light;
+mod projection;
+mod root;
+mod sky;
+mod sprites;
+mod terrain;
+mod transition;
+
+pub use light::Light;
+pub use projection::Projection;
+pub use root::RootStyleSpec;
+pub use sky::Sky;
+pub use sprites::Sprites;
+pub use terrain::Terrain;
+pub use transition::Transition;
